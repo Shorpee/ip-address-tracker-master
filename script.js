3a73178d9bdebc697b6a6daf7e6cba81c234543a
shorpee <mosopewilliamson@gmail.com>
2021-06-07 09:51:06 +0100

    feat: complete map functionality

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
 const ipAddress = document.getElementById("user-ip-address");
 const userLocation = document.getElementById("user-location");
 const timezone = document.getElementById("user-timezone");
-const isp = document.getElementById("user-isp");
+const userIsp = document.getElementById("user-isp");
+const url = "https://geo.ipify.org/api/v1?apiKey=at_NSc7fE041qkOYaoyTMBfbZu4SyJT4&ipAddress="
+let resultFinal = {};
+
 
 function getIpAddress() {
     const inputVal = document.getElementById("ip-address-input").value;
-    ipAddress.innerText = inputVal;
+    fetch(`${url}+${inputVal}`)
+        .then(response => response.json())
+        .then(result => {
+            ipAddress.innerText = result.ip;
+            userLocation.innerText = result.location.city;
+            timezone.innerText = result.location.timezone;
+            userIsp.innerText = result.isp;
+            resultFinal = {
+                lat: result.location.lat,
+                long: result.location.lng
+            }
+            initMap(resultFinal.lat, resultFinal.long);
+        })
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    // const latitude = 344;
-    // const longitude = 355;
-
+function initMap(lat, long) {
     //set initial view of the map
-    var mymap = L.map('mapid').setView([6.5244, 3.3792], 13);
+    var mymap = L.map('mapid').setView([lat, long], 17);
 
     //change marker icon
     var mapIcon = L.icon({
         iconUrl: './images/icon-location.svg',
         iconSize: [30, 38]
     });
-    L.marker([6.5525, 3.3870], {icon: mapIcon}).addTo(mymap);
+    L.marker([lat, long], { icon: mapIcon }).addTo(mymap);
 
     //set tileLayer for the map
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
+        maxZoom: 20,
         id: 'mapbox/streets-v11',
         tileSize: 512,
-        // zoomControl: false,
         zoomOffset: -1,
         accessToken: 'pk.eyJ1Ijoic2hvcnBlZTIzIiwiYSI6ImNrcGc3dXJqcTAxcWsyb244bXhqdHd6NmIifQ.-dJep14NhquKkNfvx2lrBg'
     }).addTo(mymap);
-    // L.control.attribution('false');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    getIpAddress();
 });
